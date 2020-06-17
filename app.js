99d2edc298bad6d618343de1209694c3f37c7574
GoranTurkalj <turkalj.goran@yahoo.com>
2020-06-17 21:57:52 +0200

    mobile first

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-const navList = document.querySelector(".nav__list");
 const hamburger = document.getElementById("hamburger-menu");
 
-hamburger.addEventListener("click", () => {
-  navList.classList.toggle("showMenu");
-});
+// let documentWidth = document.documentElement.offsetWidth;
 
-let documentWidth = document.documentElement.offsetWidth;
+// let elements = document.querySelectorAll("*");
 
-let elements = document.querySelectorAll("*");
-
-for (const el of elements) {
-  if (el.offsetWidth > documentWidth) {
-    console.log(el);
-  }
-}
+// for (const el of elements) {
+//   if (el.offsetWidth > documentWidth) {
+//     console.log(el);
+//   }
+// }
