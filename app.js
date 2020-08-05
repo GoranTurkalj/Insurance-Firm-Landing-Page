d999d5a555a98c79f6a05657b7133ab3cb431644
GoranTurkalj <turkalj.goran@yahoo.com>
2020-08-05 21:24:54 +0200

    fixed mobile nav

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const navList = document.querySelector(".main-nav__list");
+const body = document.querySelector("body");
+let windowWidth = window.innerWidth;
 let menuOpened = false;
 
+//Get window innerWidth on load and resize events
+window.addEventListener("load", getInnerWidth);
+window.addEventListener("resize", getInnerWidth);
+
+//Gets window innerWidth
+function getInnerWidth() {
+  windowWidth = window.innerWidth;
+  console.log(windowWidth);
+  if (menuOpened && windowWidth >= 700) {
+    menuOpened = false;
+    hideMobileNav();
+  }
+}
+
+//opens mobile nav
 function showMobileNav() {
+  menuOpened = true;
+  body.classList.add("noscroll");
   hamburgerMenu.src = "./images/icon-close.svg";
   navList.classList.add("nav-fade-in");
 }
 
+//closes mobile nav
 function hideMobileNav() {
+  menuOpened = false;
+  body.classList.remove("noscroll");
   hamburgerMenu.src = "./images/icon-hamburger.svg";
   navList.classList.add("nav-fade-out");
 
@@ -16,17 +38,25 @@ function hideMobileNav() {
   }, 500);
 }
 
+//Toggles mobile nav
 function toggleMobileNav() {
-  menuOpened = !menuOpened;
   if (menuOpened) {
-    showMobileNav();
-  } else if (!menuOpened) {
     hideMobileNav();
+  } else if (!menuOpened) {
+    showMobileNav();
   }
 }
 
 hamburgerMenu.addEventListener("click", toggleMobileNav);
 
+//Clicking on links in mobile nav will close the mobile nav
+navList.addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
+    hideMobileNav();
+  }
+});
+
+//Checks if any element causes problems by overflowing
 let documentWidth = document.documentElement.offsetWidth;
 
 let elements = document.querySelectorAll("*");
